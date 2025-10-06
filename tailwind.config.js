9d5ef1dfeaa87b8723bacdd4ea3b4395587d9f0f
jeanneryssmoreno <162622174+jeanneryssmoreno@users.noreply.github.com>
2025-10-05 23:38:48 -0400

    Revert "Feature/game logic"

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: 'class', // Habilitar dark mode basado en clase
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-}
-
